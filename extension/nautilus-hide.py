617a5f09bc364473f3ba3d2a2ee5966993b1ee3d
0xMRTT <0xMRTT@tuta.io>
2022-10-25 16:49:34 +0200

    feat: port to 43

--- extension/nautilus-hide.py
+++ extension/nautilus-hide.py
@@ -21,10 +21,10 @@ import os, subprocess
 from gi import require_version
 
 try:
-	require_version('Nautilus', '4.0')
-	require_version('Gtk', '4.0')
-	using_nautilus_43_onwards = True
-	print('Using Nautilus 43 or newer')
+    require_version('Nautilus', '4.0')
+    require_version('Gtk', '4.0')
+    using_nautilus_43_onwards = True
+    print('Using Nautilus 43 or newer')
 except:
     require_version('Nautilus', '3.0')
     require_version('Gtk', '3.0')
@@ -47,6 +47,7 @@ locale.bindtextdomain('nautilus-hide', LOCALE_DIR)
 locale.textdomain('nautilus-hide')
 
 _ = gettext.gettext
+n_ = gettext.ngettext
 
 if using_nautilus_43_onwards:
     class NautilusHide(GObject.GObject, Nautilus.MenuProvider):
@@ -54,62 +55,101 @@ if using_nautilus_43_onwards:
             print("Nautilus Hide extension initialized")
             self.is_selected = False
 
-        def get_file_items(self, files):
-            if len(files) != 1:
-                self.is_selected = False
+        def get_file_items(self, window, files):
+            """Returns the menu items to display when one or more files/folders are
+            selected."""
+            # Make "files" paths relative and remove files that start with '.'
+            # or that end with '~' (files that are already hidden)
+            dir_path = None # path of the directory
+            filenames = []
+            for file in files:
+                if dir_path == None: # first file: find path to directory
+                    dir_path = file.get_parent_location().get_path()
+                    if file.get_uri_scheme() != "file": # must be a local directory
+                        return
+                name = file.get_name()
+                if not name.startswith(".") and not name.endswith("~"):
+                    filenames += [name]
+
+            if dir_path == None or len(filenames) == 0:
                 return
 
-            self.is_selected = True
-            file = files[0]
-            items = []
-            if file.get_uri_scheme() == "file": # must be a local file/directory
-                if file.is_directory():
-                    if os.path.exists(NAUTILUS_PATH):
-                        items += [self._create_nautilus_item(file)]
+            # Check if the user has write access to the ".hidden" file and its
+            # directory
+            hidden_path = dir_path + "/.hidden" # path to the ".hidden" file
+            if not os.access(dir_path, os.W_OK | os.X_OK) or \
+            (os.path.exists(hidden_path) and not os.access(hidden_path, os.R_OK | os.W_OK)):
+                return
+
+            # Read the ".hidden" file
+            try:
+                hidden = set()
+                if os.path.exists(hidden_path):
+                    with open(hidden_path, "r", encoding="utf-8") as f:
+                        for line in f.readlines():
+                            line = line.strip("\r\n") # strip newline characters
+                            if line != "":
+                                hidden.add(line)
+            except FileNotFoundError: # ".hidden" file was deleted?
+                hidden = set()
+
+            # Determine what menu items to show (Hide, Unhide, or both)
+            show_hide, show_unhide = False, False
+            for file in filenames:
+                if file in hidden:
+                    show_unhide = True
                 else:
-                    if os.path.exists(EDITOR_PATH):
-                        items += [self._create_editor_item(file)]
-            return items
+                    show_hide = True
+                if show_hide and show_unhide:
+                    break
 
-
-        def get_background_items(self, file):
             # Add the menu items
             items = []
+            if show_hide:
+                items += [self._create_hide_item(filenames, hidden_path, hidden)]
+            if show_unhide:
+                items += [self._create_unhide_item(filenames, hidden_path, hidden)]
 
-            if self.is_selected:
-                return
-
-            if file.is_directory() and file.get_uri_scheme() == "file":
-                items += [self._create_nautilus_item(file)]
             return items
 
-        def _create_nautilus_item(self, file):
-            item = Nautilus.MenuItem(name="NautilusHide::nautilus",
-                                     label=_("Open as Admin"),
-                                     tip=_("Open this folder with root privileges"))
-            item.connect("activate", self._nautilus_run, file)
-            return item
-        
-        def _create_editor_item(self, file):
-            item = Nautilus.MenuItem(name="NautilusHide::editor",
-                                     label=_("Edit as Admin"),
-                                     tip=_("Edit this file with root privileges"))
-            item.connect("activate", self._editor_run, file)
+        def _create_unhide_item(self, files, hidden_path, hidden):
+            """Creates the 'Unhide file(s)' menu item."""
+            item = Nautilus.MenuItem(name="NautilusHide::UnhideFile",
+                                    label=n_("Un_hide File", "Un_hide Files", len(files)),
+                                    tip=n_("Unhide this file", "Unhide these files", len(files)))
+            item.connect("activate", self._unhide_run, files, hidden_path, hidden)
             return item
 
-        def _nautilus_run(self, menu, file):
-            uri = file.get_uri()
-            admin_uri = uri.replace("file://", "admin://")
-            
-            print("Openning: ",admin_uri)
-            subprocess.Popen([NAUTILUS_PATH, admin_uri])
-            
-        def _editor_run(self, menu, file):
-            uri = file.get_uri()
-            admin_uri = uri.replace("file://", "admin://")
-            
-            print("Openning: ",admin_uri)
-            subprocess.Popen([EDITOR_PATH, admin_uri])
+        def _update_hidden_file(self, hidden_path, hidden):
+            """Updates the '.hidden' file with the new filenames, or deletes it if
+            empty (no files to hide)."""
+            try:
+                if hidden == set():
+                    if os.path.exists(hidden_path):
+                        os.remove(hidden_path)
+                else:
+                    with open(hidden_path, "w", encoding="utf-8") as f:
+                        for file in hidden:
+                            f.write(file + '\n')
+                subprocess.Popen(["xdotool","key","F5"])
+            except FileNotFoundError:
+                print(f"Failed to delete or write to {hidden_path}")
+
+
+        def _hide_run(self, menu, files, hidden_path, hidden):
+            """'Hide file(s)' menu item callback."""
+            for file in files:
+                hidden.add(file)
+            self._update_hidden_file(hidden_path, hidden)
+
+        def _unhide_run(self, menu, files, hidden_path, hidden):
+            """'Unhide file(s)' menu item callback."""
+            for file in files:
+                try:
+                    hidden.remove(file)
+                except FileNotFoundError: # file not in "hidden"
+                    pass
+            self._update_hidden_file(hidden_path, hidden)
 else:
     class NautilusHide(GObject.GObject, Nautilus.MenuProvider):
         def __init__(self):
@@ -118,64 +158,99 @@ else:
             self.is_selected = False
 
         def get_file_items(self, window, files):
-            """Return to menu when click on any file/folder"""
-            if len(files) != 1:
-                self.is_selected = False
+            """Returns the menu items to display when one or more files/folders are
+            selected."""
+            # Make "files" paths relative and remove files that start with '.'
+            # or that end with '~' (files that are already hidden)
+            dir_path = None # path of the directory
+            filenames = []
+            self.window = window
+            for file in files:
+                if dir_path == None: # first file: find path to directory
+                    dir_path = file.get_parent_location().get_path()
+                    if file.get_uri_scheme() != "file": # must be a local directory
+                        return
+                name = file.get_name()
+                if not name.startswith(".") and not name.endswith("~"):
+                    filenames += [name]
+
+            if dir_path == None or len(filenames) == 0:
                 return
 
-            self.is_selected = True
-            file = files[0]
-            items = []
-            self.window = window
-            if file.get_uri_scheme() == "file": # must be a local file/directory
-                if file.is_directory():
-                    if os.path.exists(NAUTILUS_PATH):
-                        items += [self._create_nautilus_item(file)]
+            # Check if the user has write access to the ".hidden" file and its
+            # directory
+            hidden_path = dir_path + "/.hidden" # path to the ".hidden" file
+            if not os.access(dir_path, os.W_OK | os.X_OK) or \
+            (os.path.exists(hidden_path) and not os.access(hidden_path, os.R_OK | os.W_OK)):
+                return
+
+            # Read the ".hidden" file
+            try:
+                hidden = set()
+                if os.path.exists(hidden_path):
+                    with open(hidden_path, "r", encoding="utf-8") as f:
+                        for line in f.readlines():
+                            line = line.strip("\r\n") # strip newline characters
+                            if line != "":
+                                hidden.add(line)
+            except FileNotFoundError: # ".hidden" file was deleted?
+                hidden = set()
+
+            # Determine what menu items to show (Hide, Unhide, or both)
+            show_hide, show_unhide = False, False
+            for file in filenames:
+                if file in hidden:
+                    show_unhide = True
                 else:
-                    if os.path.exists(EDITOR_PATH):
-                        items += [self._create_editor_item(file)]
-            return items
+                    show_hide = True
+                if show_hide and show_unhide:
+                    break
 
-
-        def get_background_items(self, window, file):
-            """Returns the menu items to display when no file/folder is selected
-            (i.e. when right-clicking the background)."""
             # Add the menu items
             items = []
-            self.window = window
+            if show_hide:
+                items += [self._create_hide_item(filenames, hidden_path, hidden)]
+            if show_unhide:
+                items += [self._create_unhide_item(filenames, hidden_path, hidden)]
 
-            if self.is_selected:
-                return
-
-            if file.is_directory() and file.get_uri_scheme() == "file":
-                items += [self._create_nautilus_item(file)]
             return items
 
-        def _create_nautilus_item(self, file):
-            item = Nautilus.MenuItem(name="NautilusHide::nautilus",
-                                     label=_("Open as Admin"),
-                                     tip=_("Open this folder with root privileges"))
-            item.connect("activate", self._nautilus_run, file)
-            return item
-        
-        def _create_editor_item(self, file):
-            item = Nautilus.MenuItem(name="NautilusHide::editor",
-                                     label=_("Edit as Admin"),
-                                     tip=_("Edit this file with root privileges"))
-            item.connect("activate", self._editor_run, file)
+        def _create_unhide_item(self, files, hidden_path, hidden):
+            """Creates the 'Unhide file(s)' menu item."""
+            item = Nautilus.MenuItem(name="NautilusHide::UnhideFile",
+                                    label=n_("Un_hide File", "Un_hide Files", len(files)),
+                                    tip=n_("Unhide this file", "Unhide these files", len(files)))
+            item.connect("activate", self._unhide_run, files, hidden_path, hidden)
             return item
 
-        def _nautilus_run(self, menu, file):
-            uri = file.get_uri()
-            admin_uri = uri.replace("file://", "admin://")
-            
-            print("Openning: ",admin_uri)
-            subprocess.Popen([NAUTILUS_PATH, admin_uri])
-            
-        def _editor_run(self, menu, file):
-            uri = file.get_uri()
-            admin_uri = uri.replace("file://", "admin://")
-            
-            print("Openning: ",admin_uri)
-            subprocess.Popen([EDITOR_PATH, admin_uri])
+        def _update_hidden_file(self, hidden_path, hidden):
+            """Updates the '.hidden' file with the new filenames, or deletes it if
+            empty (no files to hide)."""
+            try:
+                if hidden == set():
+                    if os.path.exists(hidden_path):
+                        os.remove(hidden_path)
+                else:
+                    with open(hidden_path, "w", encoding="utf-8") as f:
+                        for file in hidden:
+                            f.write(file + '\n')
+                subprocess.Popen(["xdotool","key","F5"])
+            except FileNotFoundError:
+                print(f"Failed to delete or write to {hidden_path}")
+
+
+        def _hide_run(self, menu, files, hidden_path, hidden):
+            """'Hide file(s)' menu item callback."""
+            for file in files:
+                hidden.add(file)
+            self._update_hidden_file(hidden_path, hidden)
+
+        def _unhide_run(self, menu, files, hidden_path, hidden):
+            """'Unhide file(s)' menu item callback."""
+            for file in files:
+                try:
+                    hidden.remove(file)
+                except FileNotFoundError: # file not in "hidden"
+                    pass
+            self._update_hidden_file(hidden_path, hidden)
             
--- install_dev.sh
+++ install_dev.sh
@@ -1,3 +1,3 @@
 sudo meson builddir --prefix=/usr/local --wipe
-sudo ninja -C builddir
+sudo ninja -C builddir install
 nautilus -q
